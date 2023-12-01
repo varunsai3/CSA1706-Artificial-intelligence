bfb11355dc30ece8059df3111ad5c4df084fb552
varunsai3 <141907957+varunsai3@users.noreply.github.com>
2023-12-01 11:30:12 +0530

    Create 13 MIN MAX.py

---
+++ 13 MIN MAX.py
@@ -0,0 +1,27 @@
+import math
+
+def minimax (curDepth, nodeIndex,
+maxTurn, scores,
+targetDepth):
+
+  # base case : targetDepth reached
+  if (curDepth == targetDepth):
+      return scores[nodeIndex]
+
+  if (maxTurn):
+      return max(minimax(curDepth + 1, nodeIndex * 2,
+      False, scores, targetDepth),
+      minimax(curDepth + 1, nodeIndex * 2 + 1,
+      False, scores, targetDepth))
+
+  else:
+      return min(minimax(curDepth + 1, nodeIndex * 2,
+      True, scores, targetDepth),
+      minimax(curDepth + 1, nodeIndex * 2 + 1,
+      True, scores, targetDepth))
+
+  # Driver code
+scores = [-1,4,2,6,-3,-5,0,7]
+treeDepth = math.log(len(scores), 2)
+print("The optimal value is : ", end = "")
+print(minimax(0, 0, True, scores, treeDepth))
